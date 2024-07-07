5cf5ba9718a2b8be6757679e22ca7800a51df855
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2024-07-08 01:24:10 +0600

    added routes and pages

--- demo01/src/App.jsx
+++ demo01/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import About from './pages/about';
+import Signup from './pages/signup';
+import Signin from './pages/signin';
 
 export default function App() {
   return (
-    <h1 className='text-red-900'> App </h1>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      
+     </Routes>
+     
+    </BrowserRouter>
   )
 }
