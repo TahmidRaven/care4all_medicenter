b9d3f923dda05d872cf2ca9bb96835beb66af951
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2024-07-08 03:25:12 +0600

    connected database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); 
 
+
+mongoose
+    .connect(process.env.MONGO)
+    .then( 
+        () => 
+            {console.log('MongoDB is connected!')}
+        )
+    .catch(() => {console.log('MongoDB connection failed!')});
+        
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server listening on port 3000!');
-});
+    console.log('server listening on port 3000!!');
+}); 
