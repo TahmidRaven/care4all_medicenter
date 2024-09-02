3fbab48b9c7f9f0fe3e1b6c0047cd31cb4a41125
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2024-09-02 13:01:54 +0600

    signin works fine

--- demo01/src/pages/signin.jsx
+++ demo01/src/pages/signin.jsx
@@ -1,35 +1,71 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
-      <div className='p-6 max-w-lg w-full bg-white shadow-md rounded-lg'>
-        <h1 className='text-3xl text-center font-semibold mb-6'>Sign In</h1>
-        <form className='flex flex-col gap-4'>
-          <input
-            type='email'
-            placeholder='Email'
-            id='email'
-            className='bg-slate-100 p-3 rounded-lg'
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            id='password'
-            className='bg-slate-100 p-3 rounded-lg'
-          />
-          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-            Sign In
-          </button>
-        </form>
-        <div className='flex gap-2 mt-5 justify-center'>
-          <p>Don't have an account?</p>
-          <Link to='/signup'>
-            <span className='text-blue-500'>Sign Up</span>
-          </Link>
-        </div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/signup'>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
       </div>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
   );
 }
