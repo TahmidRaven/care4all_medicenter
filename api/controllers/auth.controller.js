e0a836e227e085dd8ec2a7983dfc39486ce8353c
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2024-09-13 12:32:22 +0600

    new auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,8 +3,6 @@ import crypto from 'crypto';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
- 
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -46,6 +44,51 @@ export const signin = async (req, res, next) => {
       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
       .status(200)
       .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const salt = crypto.randomBytes(16).toString('hex');
+      const hashedPassword = crypto.scryptSync(generatedPassword, salt, 64).toString('hex');
+      const newUser = new User({
+        username:
+          req.body.name.split(' ').join('').toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: `${salt}:${hashedPassword}`,
+        profilePicture: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
