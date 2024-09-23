92ee461fc7cb320e52a3a084c9c5c6745f29e707
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2024-09-23 09:36:16 +0600

    Add files via upload

--- demo01/src/App.jsx
+++ demo01/src/App.jsx
@@ -6,12 +6,30 @@ import Signup from './pages/signup';
 import Signin from './pages/signin';
 import Header from './components/header';
 import Footer from './components/footer';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/profile';
+import backgroundWall from './assets/backgroundWall.jpg'; // Import background image
+
+import AdminSignup from './pages/adminsignup';
+import AdminSignin from './pages/adminsignin';
+import AdminDashboard from './pages/admin';
+import AppointmentScheduling from './pages/AppointmentScheduling';
+import AppointmentManagement from './pages/AppointmentManagement';
+import FeedbackRatings from './pages/FeedbackRatings';
+import Donations from './pages/Donations'; 
+ 
+
 
 export default function App() {
   return (
     <BrowserRouter>
       {/* Using flex container to ensure footer stays at the bottom */}
-      <div className="flex flex-col min-h-screen">
+
+      <div
+        className="min-h-screen flex flex-col bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundWall})` }}
+      >
+
         {/* Header */}
         <Header />
 
@@ -22,6 +40,18 @@ export default function App() {
             <Route path="/about" element={<About />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
+            <Route element={<PrivateRoute />}>
+              <Route path='/profile' element={<Profile />} />
+             </Route>
+            <Route path="/adminsignup" element={<AdminSignup />} />
+            <Route path="/adminsignin" element={<AdminSignin />} />
+            <Route path='/admin/' element={<AdminDashboard />} />
+            <Route path="/appointment-scheduling" element={<AppointmentScheduling />} />
+            <Route path="/appointment-management" element={<AppointmentManagement />} />
+            <Route path="/feedback-ratings" element={<FeedbackRatings />} />
+            <Route path="/donations" element={<Donations />} />
+
+
           </Routes>
         </main>
 
