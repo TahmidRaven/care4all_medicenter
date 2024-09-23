93ab15af198638df655b8a6d2d1e9896c1275900
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2024-09-23 11:52:47 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,20 +4,16 @@ import dotenv from 'dotenv';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import adminRoutes from './routes/admin.route.js';
-import donationRoutes from './routes/donationRoutes.js';
-import feedbackRoutes from './routes/feedbackRoutes.js';
-import appointmentRoutes from './routes/appointmentRoutes.js';
+import adminRoutes from './routes/admin.route.js'; 
 
 import cookieParser from 'cookie-parser';
 
-dotenv.config();
+
+
+dotenv.config(); 
 
 mongoose
-  .connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -30,25 +26,22 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Integrate routes
+app.listen(3000, () => {
+  console.log('Server listening on port 3000!!');
+});
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/donations', donationRoutes);
-app.use('/api/feedbacks', feedbackRoutes);
-app.use('/api/appointments', appointmentRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req,res, next) => { 
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     message,
-  });
-});
+    statusCode,
+  })
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!!`);
-});
+}
+)
